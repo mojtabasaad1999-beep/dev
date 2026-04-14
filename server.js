a7c7dbc75ab538fc80e4d35f2bbfd6eceb527d21
Mojtaba Saad <mojtaba.saad1999@gmail.com>
2026-04-14 21:57:43 +0200

    secure email config

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "mojtaba.saad1999@gmail.com",
-    pass: "ksmpvxvnildhqzuw"
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
