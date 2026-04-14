0864caba645bb0e8cd2ad37eb4f8e2f5ba95ec51
Mojtaba Saad <mojtaba.saad1999@gmail.com>
2026-04-14 22:13:51 +0200

    fix smtp port

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const nodemailer = require("nodemailer");
 const app = express();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // VERY IMPORTANT (must be false for 587)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
